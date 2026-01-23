c750a1c1e7072a96122588cee9cbf069e68e9fcf
Anil Yadav <rebelanil885@gmail.com>
2026-01-23 12:16:58 +0530

    spiral matrix

---
+++ Medium/SpiralMat.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int left = 0, top = 0;
+        int right = matrix[0].length - 1;
+        int bottom = matrix.length - 1;
+
+        while (left <= right && top <= bottom) {
+
+            // left to right
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+            // top to bottom
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+            // right to left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // bottom to top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+}
