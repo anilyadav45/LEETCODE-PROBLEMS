a16c95621280055cf8b679943b0dc927a8020229
Anil Yadav <rebelanil885@gmail.com>
2026-03-29 12:29:00 +0530

    optimal matrix 2d

--- Medium/SearchIn2DArr.java
+++ Medium/SearchIn2DArr.java
@@ -1,3 +1,4 @@
+//better 
 class Solution {
     private static boolean binSearch(int arr[], int target) {
         int low = 0;
@@ -23,6 +24,30 @@ class Solution {
             if (col == true) {
                 return true;
             }
+        }
+        return false;
+    }
+}
+
+//best optimal - row eliminate and col eliminate 
+//start from 1st row and last col elem
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //keep in mind we are starting from first row and last col so ,  we eliminate row and col 
+        //so while elimination like for row move next for col backward becuase row = 0 and col = n - 1; 
+        int row = 0;//first row
+        int col = matrix[0].length - 1;//lasat col
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            //col is sorted so no need to move col but can be in this row since elem is larger so eliminate col last
+            if (matrix[row][col] > target) {
+                col--;
+            } else if (matrix[row][col] < target) {
+                //since curr elem is lower so need to move bigger so row eliminate and move to next row
+                row++;
+            } 
         }
         return false;
     }
